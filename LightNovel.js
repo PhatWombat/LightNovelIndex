ab2141e903c9cd301ee01083bf323123f22a1796
PhatWombat <50849929+PhatWombat@users.noreply.github.com>
2019-12-04 04:26:23 -0500

    Add files via upload

---
+++ LightNovel.js
@@ -0,0 +1,28 @@
+
+/*var x, y;
+x = 5;
+y = 6;
+document.getElementById("demo").innerHTML = x + y;*/
+
+/*				
+function Ookamie() {
+	document.getElementById('myImage').src='Images/Hollo04.gif';
+	document.getElementById('BtnHollo').style.display='none';
+	document.getElementById('BtnBunnies').style.display='block';
+	}
+	
+function Bunnies() {
+	document.getElementById('myImage').src='Images/Bunnies.png';
+	document.getElementById('BtnBunnies').style.display='none';
+	document.getElementById('BtnHollo').style.display='block';
+	}
+	document.getElementById('BunnieHollo').style.display='none';
+*/
+
+
+function funcAccordeon0() {
+	$( "#accordion" ).accordion({ heightStyle: "content", collapsible: true, active : false /*0*/});
+} 
+
+	
+	
