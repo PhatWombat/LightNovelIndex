0b6111e1d33e742e5debbc3db3b8e3d637e7a8f4
PhatWombat <50849929+PhatWombat@users.noreply.github.com>
2020-02-09 02:26:52 -0500

    Update LightNovel.js

--- LightNovel.js
+++ LightNovel.js
@@ -1,23 +1,22 @@
 
-/*var x, y;
-x = 5;
-y = 6;
-document.getElementById("demo").innerHTML = x + y;*/
+var imgBankCornerAll =	["https://cdn.discordapp.com/attachments/652804954083426304/652805005971030016/Aqua_Dance.gif",
+			"https://cdn.discordapp.com/attachments/652804954083426304/652817184325894157/Darkness_Dance02.gif",
+			"https://cdn.discordapp.com/attachments/652804954083426304/652814953820192768/Megumin_Dance.gif",
+			"https://cdn.discordapp.com/attachments/652804954083426304/652817567102271498/Kazuma_Dance.gif",
+			"https://media.discordapp.net/attachments/652804954083426304/675916313910312989/Yume01.1.png"];
 
-/*				
-function Ookamie() {
-	document.getElementById('myImage').src='Images/Hollo04.gif';
-	document.getElementById('BtnHollo').style.display='none';
-	document.getElementById('BtnBunnies').style.display='block';
-	}
-	
-function Bunnies() {
-	document.getElementById('myImage').src='Images/Bunnies.png';
-	document.getElementById('BtnBunnies').style.display='none';
-	document.getElementById('BtnHollo').style.display='block';
-	}
-	document.getElementById('BunnieHollo').style.display='none';
-*/
+
+var imgBankCornerRight =   ["https://cdn.discordapp.com/attachments/635375905077985284/645852617372860416/Raph_Chan.png",
+			   "https://cdn.discordapp.com/attachments/652804954083426304/675907868628746250/Chomusuke_Quartet.png",
+			   "https://cdn.discordapp.com/attachments/652804954083426304/675964133342183432/Yume02.2.png"];
+
+
+var imgBankCornerLeft = ["https://cdn.discordapp.com/attachments/635375905077985284/647309483210440726/Raph_Chan_Smoll.png",
+			"https://cdn.discordapp.com/attachments/652804954083426304/656773994195976212/Emperor_Zel_500.png"];
+
+
+
+
 
 
 function funcAccordeon0() {
@@ -88,24 +87,6 @@ function randomCornerDroit() {							//change l'image de droite avec une image d
 }
 
 
-
-
-
-
-var imgBankCornerAll =	["https://cdn.discordapp.com/attachments/652804954083426304/652805005971030016/Aqua_Dance.gif",
-						"https://cdn.discordapp.com/attachments/652804954083426304/652817184325894157/Darkness_Dance02.gif",
-						"https://cdn.discordapp.com/attachments/652804954083426304/652814953820192768/Megumin_Dance.gif",
-						"https://cdn.discordapp.com/attachments/652804954083426304/652817567102271498/Kazuma_Dance.gif",
-						"https://media.discordapp.net/attachments/652804954083426304/675916313910312989/Yume01.1.png"];
-
-
-var imgBankCornerRight =   ["https://cdn.discordapp.com/attachments/635375905077985284/645852617372860416/Raph_Chan.png",
-			   "https://cdn.discordapp.com/attachments/652804954083426304/675907868628746250/Chomusuke_Quartet.png",
-			   "https://cdn.discordapp.com/attachments/652804954083426304/675910664694399016/Chomusuke_Quartet02.png"];
-
-
-var imgBankCornerLeft = ["https://cdn.discordapp.com/attachments/635375905077985284/647309483210440726/Raph_Chan_Smoll.png",
-						"https://cdn.discordapp.com/attachments/652804954083426304/656773994195976212/Emperor_Zel_500.png"];
 
 
 /*---------------------------------------------------------------NSFW Bank--------------------------------------------------------------------------------------*/
